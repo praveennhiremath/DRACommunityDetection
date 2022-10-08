fb5bdbf5e676fa907ca0709dcf1e0fbca1f16792
Praveen Hiremath <praveen.kumar.hiremath@oracle.com>
2022-10-08 11:43:07 +0530

    Reading the db configurations and graph inputs from the property files.

--- src/main/java/com/oracle/ms/app/InfomapGraphClient.java
+++ src/main/java/com/oracle/ms/app/InfomapGraphClient.java
@@ -1,8 +1,14 @@
 package com.oracle.ms.app;
 
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.Set;
 import java.util.concurrent.ExecutionException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import com.google.common.base.Supplier;
 
@@ -25,12 +31,13 @@ public class InfomapGraphClient {
 
 	public static void main(String[] args) throws ExecutionException, InterruptedException {
 		
+		FileProperties properties = new FileProperties();
 		var config = AdbGraphClientConfiguration.builder();
-		config.tenant("ocid1.tenancy.oc1..aaaaaaaabls4dzottlktt774tu3knax6crpozycjhrqpm73thfryxwlkmkba");
-		config.database("medicalrecordsdb");
-		config.username("ADMIN");
-		config.password("Welcome12345");
-		config.endpoint("https://bsenjiat5lmurtq-medicalrecordsdb.adb.us-ashburn-1.oraclecloudapps.com/");
+		config.tenant(properties.readProperty(Constants.TENANT_STR));
+		config.database(properties.readProperty(Constants.DATABASE_STR));
+		config.username(properties.readProperty(Constants.DB_USERNAME_STR));
+		config.password(properties.readProperty(Constants.DB_PASSWORD_STR));
+		config.endpoint(properties.readProperty(Constants.DB_ENDPOINT_STR));
 		
 		var client = new AdbGraphClient(config.build());
 		
@@ -41,61 +48,65 @@ public class InfomapGraphClient {
 		}
 
 		ServerInstance instance = client.getPgxInstance();
-		PgxSession session = instance.createSession("MedicalRecAdbGraphSessionName");
+		PgxSession session = instance.createSession(Constants.SESSION_NAME_STR);
 		//MED_REC_PG_OBJ_G
 		//MED_RECS_NEW_G - 259 nodes
 		//PgxGraph graph = session.readGraphByName("MED_RECS_NEW_G", oracle.pgx.api.GraphSource.PG_VIEW);
-		//PgxGraph graph = session.getGraph("MED_REC_PG_OBJ_G");
 		
-		/*
-		 * Supplier<GraphConfig> pgxConfig = () -> { return
-		 * GraphConfigBuilder.forPropertyGraphRdbms() .setName("hr")
-		 * 
-		 * .setLoadVertexLabels(true) .setLoadEdgeLabel(true) .build(); };
-		 */
-		
-		/*
-		 * GraphConfig graphConfig = AdbGraphClientConfiguration.builder(); graph =
-		 * session.readGraphWithProperties(config);
-		 */
 		
 		GraphConfig graphConfig = 
 				GraphConfigBuilder.forPropertyGraphRdbms()
-				.setName("MED_REC_PG_OBJ_G")
-				.addVertexProperty("TABLE_NAME", PropertyType.STRING)
-				.addEdgeProperty("TABLE1", PropertyType.STRING)
-				.addEdgeProperty("TABLE2", PropertyType.STRING)
-				.addEdgeProperty("TOTAL_AFFINITY", PropertyType.DOUBLE)
-		
-		// .setPartitionWhileLoading(PartitionWhileLoading.BY_LABEL)
-		 .setLoadVertexLabels(true)
-		 .setLoadEdgeLabel(true)
-		 .build();
+				.setName(properties.readGraphProperty(Constants.GRAPH_NAME_STR))
+				.addVertexProperty(properties.readGraphProperty(Constants.VERTEX_COLUMN_STR), PropertyType.STRING)
+				.addEdgeProperty(properties.readGraphProperty(Constants.EDGE_SOURCE_COL_STR), PropertyType.STRING)
+				.addEdgeProperty(properties.readGraphProperty(Constants.EDGE_DESTINATION_COL_STR), PropertyType.STRING)
+				.addEdgeProperty(properties.readGraphProperty(Constants.EDGE_WEIGHT_COL_STR), PropertyType.DOUBLE)
+				.setLoadVertexLabels(true)
+				.setLoadEdgeLabel(true)
+				.build();
 		PgxGraph graph = session.readGraphWithProperties(graphConfig);
 		
 		System.out.println("Graph : " + graph);
 		Analyst analyst = session.createAnalyst();
-		EdgeProperty<Double> weight = graph.getEdgeProperty("TOTAL_AFFINITY");
+		
+		// Default Max Iteration for Infomap is set to 1.
+		int maxIterations = 1;
+		String targetCommunityTableName = null;
+		try {
+			if (args.length > 0) {
+				maxIterations = Integer.parseInt(args[0]);
+			}
+			if (args.length > 1) {
+				targetCommunityTableName = args[1];
+			}
+		} catch (ArrayIndexOutOfBoundsException aiex) {
+			aiex.printStackTrace();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		
+		EdgeProperty<Double> weight = graph.getEdgeProperty(properties.readGraphProperty(Constants.EDGE_WEIGHT_COL_STR));
 		try {
 			VertexProperty<Integer, Double> rank = analyst.weightedPagerank(graph, 1e-16, 0.85, 1000, true, weight);
-			VertexProperty<Integer, Long> module = graph.createVertexProperty(PropertyType.LONG,"Community");
-			Partition<Integer> promise = analyst.communitiesInfomap(graph, rank, weight, 0.15, 0.0001, 1, module);
-			Set<VertexProperty<?, ?>> vProps = graph.getVertexProperties();
-			for (VertexProperty<?,?> prop : vProps) {
-				System.out.println("Prop : " + prop.getName());
+			VertexProperty<Integer, Long> module = graph.createVertexProperty(PropertyType.LONG, Constants.COMMUNITY_STR);
+			System.out.println("Calling Infomap with Max Iterations = " + maxIterations);
+			Partition<Integer> promise = analyst.communitiesInfomap(graph, rank, weight, 0.15, 0.0001, maxIterations, module);
+			graph.queryPgql("SELECT n." + Constants.COMMUNITY_STR + ",n.TABLE_NAME FROM MATCH (n) order by n." + Constants.COMMUNITY_STR + "").print().close();
+			if (targetCommunityTableName != null) {
+				System.out.println("Below are the Nodes of Selected Community : " + targetCommunityTableName);
+				graph.queryPgql("SELECT n." + Constants.COMMUNITY_STR + " FROM MATCH (n) where n.TABLE_NAME = '" + targetCommunityTableName + "'").print().close();
+				System.out.println("SELECT n.TABLE_NAME FROM MATCH (n) WHERE n." + Constants.COMMUNITY_STR + " IN (SELECT n." + Constants.COMMUNITY_STR + " FROM MATCH (n) where n.TABLE_NAME = '" + targetCommunityTableName + "')");
+				graph.queryPgql("SELECT n.TABLE_NAME FROM MATCH (n) WHERE n." + Constants.COMMUNITY_STR + " IN (SELECT n." + Constants.COMMUNITY_STR + " FROM MATCH (n) where n.TABLE_NAME = '" + targetCommunityTableName + "')").print().close();
 			}
-			graph.queryPgql("SELECT n.Community,n.TABLE_NAME FROM MATCH (n) order by n.Community").print().close();
 			
-			/*VertexCollection<Integer> first_component = promise.getPartitionByIndex(0);
-
-			System.out.println("first_component : " + first_component);
-			
-			for (VertexCollection<Integer> partition : promise){
+			/*
+			for (VertexCollection<Integer> partition : promise) {
 			    System.out.println("======== ===== =====partition : " + partition);
 			    for (PgxVertex<Integer> vertexInCommunity : partition){
 			    	System.out.println("Res : " + vertexInCommunity.getProperty("TABLE_NAME"));
 			    }
-			}*/
+			}
+			*/
 		} catch (ExecutionException e) {
 			e.printStackTrace();
 		} catch (InterruptedException e) {
